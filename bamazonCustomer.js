259e3140cfab68410daa42a619bfd47bfef143ff
Simon Nguyen <simonnguyen3054@gmail.com>
2018-08-08 13:28:50 -0700

    Update items after purchase

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -25,9 +25,53 @@ function runApp() {
   console.log('App is running...');
 
   connection.query(
-    'SELECT item_id, product_name, price FROM products',
+    'SELECT item_id, product_name, price, stock_quantity FROM products',
     function(err, products) {
-      console.log(products);
+      console.log("Here's a list of items we carry in our store:\n");
+      for (var i = 0; i < products.length; i++) {
+        console.log(
+          `Item ID: ${products[i].item_id} | Product Name: ${
+            products[i].product_name
+          } | Price: $${products[i].price}\n`
+        );
+      }
+
+      inquirer
+        .prompt([
+          {
+            type: 'input',
+            name: 'product_id',
+            message: 'Input a product id that you would like to buy:'
+          },
+          {
+            type: 'input',
+            name: 'quantity',
+            message: 'How many units would you like to buy?'
+          }
+        ])
+        .then(function(data) {
+          for (var i = 0; i < products.length; i++) {
+            if (
+              products[i].item_id == data.product_id &&
+              products[i].stock_quantity > data.quantity
+            ) {
+              console.log('We have enough stock', );
+              console.log(`Order Summary: Quantity = ${data.quantity} | Total Price: $${data.quantity * products[i].price}`);
+              var remaining_stock_quantity = products[i].stock_quantity - data.quantity;
+              var query = connection.query(
+                'UPDATE products SET ? WHERE ?',
+                [
+                  {stock_quantity: remaining_stock_quantity},
+                  {item_id: data.product_id}
+                ],
+                function(err, res) {
+                  console.log("Thank you for your business!");
+                }
+              );
+            }
+          }
+          connection.end();
+        });
     }
   );
 }
